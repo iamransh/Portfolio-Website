dff3827e1d2cb41dfa79c5fd44a5029201420cf1
Prakhar Kapoor <kapoorprakhar99@gmail.com>
2023-10-03 23:24:30 +0530

    added dynamic import to herosection

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Navbar from "./Navbar";
-import HeroSection from "./HeroSection";
+import dynamic from "next/dynamic";
+// import HeroSection from "./HeroSection";
+const HeroSection = dynamic(() => import("./HeroSection"), { ssr: false });
 import About from "./About";
 import Skills from "./Skills";
 
